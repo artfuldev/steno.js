d8ed2823f2530b98d0a2b7a1ace27c76b85ccf4d
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-21 02:26:51 +0530

    Modified Unit Tests to reflect Refactoring
    Also removed Tests for html and render, since they've not been
    imlemented.

--- js/tests/tests.js
+++ js/tests/tests.js
@@ -5,18 +5,18 @@
 
 $Q.module("Single Element Extractors");
 
-$Q.test('Element', function (assert) {
-    assert.throws(function () { $Z.element(); }, new Error('Incorrect Number of Arguments'), 'Error thrown when no input is passed');
-    assert.throws(function () { $Z.element(1, 2); }, new Error('Incorrect Number of Arguments'), 'Error thrown when more than 1 input is passed');
-    assert.throws(function () { $Z.element(undefined); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Undefined');
-    assert.throws(function () { $Z.element(null); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Null');
-    assert.throws(function () { $Z.element([]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
-    assert.throws(function () { $Z.element({}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
-    assert.throws(function () { $Z.element(1); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
-    assert.deepEqual($Z.element('work'), 'work', 'Proper reply when valid arguments are passed - string');
-    assert.deepEqual($Z.element(new String('work')), 'work', 'Proper reply when valid arguments are passed - String');
+$Q.test('Name', function (assert) {
+    assert.throws(function () { $Z.name(); }, new Error('Incorrect Number of Arguments'), 'Error thrown when no input is passed');
+    assert.throws(function () { $Z.name(1, 2); }, new Error('Incorrect Number of Arguments'), 'Error thrown when more than 1 input is passed');
+    assert.throws(function () { $Z.name(undefined); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Undefined');
+    assert.throws(function () { $Z.name(null); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Null');
+    assert.throws(function () { $Z.name([]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
+    assert.throws(function () { $Z.name({}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
+    assert.throws(function () { $Z.name(1); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
+    assert.deepEqual($Z.name('work'), 'work', 'Proper reply when valid arguments are passed - string');
+    assert.deepEqual($Z.name(new String('work')), 'work', 'Proper reply when valid arguments are passed - String');
     for (var i in selectors)
-        assert.deepEqual($Z.element(selectors[i].string), selectors[i].element, 'Element in \'' + selectors[i].string + '\' is \'' + selectors[i].element + '\'');
+        assert.deepEqual($Z.name(selectors[i].string), selectors[i].name, 'Element in \'' + selectors[i].string + '\' is \'' + selectors[i].name + '\'');
 });
 $Q.test('Id', function (assert) {
     assert.throws(function () { $Z.id(); }, new Error('Incorrect Number of Arguments'), 'Error thrown when no input is passed');
@@ -39,8 +39,8 @@ $Q.test('Classes', function (assert) {
     assert.throws(function () { $Z.classes([]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
     assert.throws(function () { $Z.classes({}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
     assert.throws(function () { $Z.classes(1); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
-    assert.deepEqual($Z.classes('work'), '', 'Proper reply when valid arguments are passed - string');
-    assert.deepEqual($Z.classes(new String('work')), '', 'Proper reply when valid arguments are passed - String');
+    assert.deepEqual($Z.classes('work'), [], 'Proper reply when valid arguments are passed - string');
+    assert.deepEqual($Z.classes(new String('work')), [], 'Proper reply when valid arguments are passed - String');
     for (var i in selectors)
         assert.deepEqual($Z.classes(selectors[i].string), selectors[i].classes, 'Classes in \'' + selectors[i].string + '\' are \'' + selectors[i].classes + '\'');
 });
@@ -52,56 +52,8 @@ $Q.test('Attributes', function (assert) {
     assert.throws(function () { $Z.attributes([]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
     assert.throws(function () { $Z.attributes({}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
     assert.throws(function () { $Z.attributes(1); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
-    assert.deepEqual($Z.attributes('work'), '', 'Proper reply when valid arguments are passed - string');
-    assert.deepEqual($Z.attributes(new String('work')), '', 'Proper reply when valid arguments are passed - String');
+    assert.deepEqual($Z.attributes('work'), {}, 'Proper reply when valid arguments are passed - string');
+    assert.deepEqual($Z.attributes(new String('work')), {}, 'Proper reply when valid arguments are passed - String');
     for (var i in selectors)
         assert.deepEqual($Z.attributes(selectors[i].string), selectors[i].attributes, 'Attributes in \'' + selectors[i].string + '\' are \'' + selectors[i].attributes + '\'');
-});
-
-$Q.module("DOM Extractors");
-
-$Q.test('Html', function (assert) {
-    assert.expect(19);
-    assert.throws(function () { $Z.html(); }, new Error('Incorrect Number of Arguments'), 'Error thrown when no input is passed');
-    assert.throws(function () { $Z.html(1, 2, 3); }, new Error('Incorrect Number of Arguments'), 'Error thrown when more than 2 inputs are passed');
-    assert.throws(function () { $Z.html(undefined, undefined); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Undefined');
-    assert.throws(function () { $Z.html(null, null); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Null');
-    assert.throws(function () { $Z.html([],[]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
-    assert.throws(function () { $Z.html({}, {}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
-    assert.throws(function () { $Z.html(1,2); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
-    assert.deepEqual($Z.html('work', {}), '<work></work>', 'Proper HTML reply when valid arguments are passed - string');
-    assert.deepEqual($Z.html(new String('work'), {}), '<work></work>', 'Proper HTML reply when valid arguments are passed - String');
-    assert.deepEqual($Z.html('', {}), '<div></div>', 'Empty Elements mean divs');
-    assert.deepEqual($Z.html('work#menu', {}), '<work id="menu"></work>', 'Proper HTML when id is passed');
-    assert.deepEqual($Z.html('work.menu', {}), '<work class="menu"></work>', 'Proper HTML when class is passed');
-    assert.deepEqual($Z.html('work.dropdown.dropdown-menu', {}), '<work class="dropdown dropdown-menu"></work>', 'Proper HTML when multiple classes are passed');
-    assert.deepEqual($Z.html('work#menu.dropdown.dropdown-menu', {}), '<work id="menu" class="dropdown dropdown-menu"></work>', 'Proper HTML when id and classes are passed together');
-    assert.deepEqual($Z.html('work.dropdown#menu.dropdown-menu', {}), '<work id="menu" class="dropdown dropdown-menu"></work>', 'Proper HTML when order is not maintained');
-    assert.deepEqual($Z.html('.dropdown#menu.dropdown-menu', {}), '<div id="menu" class="dropdown dropdown-menu"></div>', 'Empty Elements mean divs when classes and id are provided');
-    assert.deepEqual($Z.html('work', {prefix:'<div>'}), '<div><work></work>', 'Proper HTML reply when prefix is passed');
-    assert.deepEqual($Z.html('work#menu', { suffix: '</div>' }), '<work id="menu"></work></div>', 'Proper HTML reply when suffix is passed');
-    assert.deepEqual($Z.html('work.dropdown#menu.dropdown-menu',
-        {
-            prefix: '<div class="prefix">',
-            suffix: '</div>'
-        }), '<div class="prefix"><work id="menu" class="dropdown dropdown-menu"></work></div>', 'Proper HTML when both prefix and suffix are passed');
-});
-$Q.test('Render', function(assert) {
-    assert.expect(16);
-    assert.throws(function () { $Z.render(); }, new Error('Incorrect Number of Arguments'), 'Error thrown when no input is passed');
-    assert.throws(function () { $Z.render(1, 2); }, new Error('Incorrect Number of Arguments'), 'Error thrown when more than 1 input is passed');
-    assert.throws(function () { $Z.render(undefined); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Undefined');
-    assert.throws(function () { $Z.render(null); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Null');
-    assert.throws(function () { $Z.render([]); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Array');
-    assert.throws(function () { $Z.render({}); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Object');
-    assert.throws(function () { $Z.render(1); }, new Error('Invalid Arguments'), 'Error thrown when invalid input is passed - Number');
-    assert.deepEqual($Z.render('work'), '<work></work>', 'Proper HTML reply when valid arguments are passed - string');
-    assert.deepEqual($Z.render(new String('work')), '<work></work>', 'Proper HTML reply when valid arguments are passed - String');
-    assert.deepEqual($Z.render(''), '<div></div>', 'Empty Elements mean divs');
-    assert.deepEqual($Z.render('work#menu'), '<work id="menu"></work>', 'Proper HTML when id is passed');
-    assert.deepEqual($Z.render('work.menu'), '<work class="menu"></work>', 'Proper HTML when class is passed');
-    assert.deepEqual($Z.render('work.dropdown.dropdown-menu'), '<work class="dropdown dropdown-menu"></work>', 'Proper HTML when multiple classes are passed');
-    assert.deepEqual($Z.render('work#menu.dropdown.dropdown-menu'), '<work id="menu" class="dropdown dropdown-menu"></work>', 'Proper HTML when id and classes are passed together');
-    assert.deepEqual($Z.render('work.dropdown#menu.dropdown-menu'), '<work id="menu" class="dropdown dropdown-menu"></work>', 'Proper HTML when order is not maintained');
-    assert.deepEqual($Z.render('.dropdown#menu.dropdown-menu'), '<div id="menu" class="dropdown dropdown-menu"></div>', 'Empty Elements mean divs when classes and id are provided');
 });
