3f4662c6679a8faf7fefe7a9fdc60184ffeeacc5
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-20 10:19:22 +0530

    Added some helpers

--- js/project/zen-query.js
+++ js/project/zen-query.js
@@ -29,16 +29,17 @@
         render: function (string) {
             if (arguments.length != 1)
                 throw new Error('Incorrect Number of Arguments');
-            if (typeof string == 'undefined' || string == null || (typeof string != 'string' && !(string instanceof String)))
+            if (!ZenQuery.is('string',string))
                 throw new Error('Invalid Arguments');
+            var matches = config.matches;
             //If no element, make div the default element
-            var element = string.match(/^[a-z-]+/);
+            var element = string.match(matches.element);
             if (!element)
                 element = 'div';
-            var classes = string.match(/\.[a-z-]+/g);
+            var classes = string.match(matches.classes);
             if (classes && classes.length > 0)
                 classes = ' class="' + classes.join(' ').replace(/\./g, '') + '"';
-            var id = string.match(/#[a-z-]+/);
+            var id = string.match(matches.id);
             if (id && id.length > 0)
                 id = ' id="' + id.join('').replace('#', '') + '"';
             var prefix = '<' + element;
@@ -64,30 +65,145 @@
         ZenQuery.constructor = F;
     }());
 
-    //Extend
-    function extend(a, b) {
-        for (var prop in b) {
-            if (hasOwn.call(b, prop)) {
-                // Avoid "Member not found" error in IE8 caused by messing with window.constructor
-                if (!(prop === "constructor" && a === window)) {
-                    if (b[prop] === undefined) {
-                        delete a[prop];
-                    } else {
-                        a[prop] = b[prop];
+    //Extend - from jQuery
+    function extend() {
+        var options, name, src, copy, copyIsArray, clone,
+            target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+
+            // skip the boolean and the target
+            target = arguments[ i ] || {};
+            i++;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+            target = {};
+        }
+
+        // extend jQuery itself if only one argument is passed
+        if ( i === length ) {
+            target = this;
+            i--;
+        }
+
+        for ( ; i < length; i++ ) {
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null ) {
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && jQuery.isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = jQuery.extend( deep, clone, copy );
+
+                        // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
                     }
                 }
             }
         }
-        return a;
-    }
+
+        // Return the modified object
+        return target;
+    };
 
     //Config
     config = {
-        symbols: ['#', '.'],
+        matches: {
+            element: /^[a-z-]+/,
+            id: /#[a-z-]+/,
+            classes: /\.[a-z-]+/g
+        },
+        html: {
+            prefix: '',
+            suffix: ''
+        }
     };
 
-    //Add config to ZenQUery
-    extend(ZenQuery, { config: config });
+    //Add stuff to ZenQuery
+    extend(ZenQuery, {
+
+        //config object
+        config: config,
+
+        // Safe object type checking - from QUnit
+        is: function (type, obj) {
+            return QUnit.objectType(obj) === type;
+        },
+        objectType: function (obj) {
+            if (typeof obj === "undefined") {
+                return "undefined";
+            }
+
+            // Consider: typeof null === object
+            if (obj === null) {
+                return "null";
+            }
+
+            var match = toString.call(obj).match(/^\[object\s(.*)\]$/),
+                type = match && match[1] || "";
+
+            switch (type) {
+                case "Number":
+                    if (isNaN(obj)) {
+                        return "nan";
+                    }
+                    return "number";
+                case "String":
+                case "Boolean":
+                case "Array":
+                case "Date":
+                case "RegExp":
+                case "Function":
+                    return type.toLowerCase();
+            }
+            if (typeof obj === "object") {
+                return "object";
+            }
+            return undefined;
+        },
+
+        //extend
+        extend: extend,
+    });
+
+    // Add private stuff to ZenQuery
+    extend(ZenQuery.constructor.prototype, {
+
+        // Complex render method
+        html: function (string, options) {
+            var html = '';
+            if (hasOwn.call(options, 'prefix') || hasOwn.call(options, 'suffix')) {
+                var defaults = ZenQuery.config.html;
+                html = extend({}, defaults, options);
+            }
+            return html;
+        }
+    });
 
     // For browser, export only select globals
     if (typeof window !== "undefined" && window != null) {
