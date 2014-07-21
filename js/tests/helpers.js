8adbc86bad15d3743d9eaee2c0d8633c7c5e7565
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-21 23:39:12 +0530

    Modified helpers to accomodate html()

--- js/tests/helpers.js
+++ js/tests/helpers.js
@@ -9,11 +9,11 @@ var ids = ['','#menu'];
 var elements = ['', 'div', 'p'];
 var attributes = [
     '', '[data-attribute]', '[for=""]', '[href="github.com/#"]', '[title="Something about ZenQuery\'s Awesomeness"]',
-    '[filter][title="Something about ZenQuery\'s Awesomeness"]', '[filter title="Something about ZenQuery\'s Awesomeness"]'
+    '[filter][title="Something about ZenQuery\'s Awesomeness"]', '[filter title="Something about ZenQuery\'s Awesomeness" class="haha" id="hehe"]'
 ];
 var attributesObjects = [
     {}, { 'data-attribute': '' }, { 'for': '' }, { href: 'github.com/#' }, { title: "Something about ZenQuery\'s Awesomeness" },
-    { filter: '', title: "Something about ZenQuery\'s Awesomeness" }, { filter: '', title: "Something about ZenQuery\'s Awesomeness" }
+    { filter: '', title: "Something about ZenQuery\'s Awesomeness" }, { filter: '', title: "Something about ZenQuery\'s Awesomeness", 'class':'haha', id: 'hehe' }
 ];
 
 var selectors = [];
@@ -21,35 +21,52 @@ for (var i in elements)
     for (var j in ids)
         for (var k in classes)
             for (var l in attributes) {
-                var classArray = $Z.trim(classes[k].replace(/\./g, ' ')).split(' ');
-                if (classArray.length === 1 && classArray[0] === '')
+                var classArray = $Z.trim(classes[k].replace(/\./g, ' ')).split(' '),
+                    attributesObject = ZenQuery.extend(true, {}, attributesObjects[l]),
+                    id = ids[j].replace('#', '');
+                if (classArray.length === 1 && classArray[0] === '') {
                     classArray = [];
+                } else {
+                    if (!attributesObject['class']) {
+                        attributesObject['class'] = classArray.join(' ');
+                    } else {
+                        attributesObject['class'] += ' ' + classArray.join(' ');
+                    }
+                }
+                if (id) {
+                    ZenQuery.extend(true, attributesObject, { id: id });
+                }
+                var element = {
+                    parent: null,
+                    name: elements[i] == '' ? 'div' : elements[i],
+                    attributes: attributesObject,
+                    children: [],
+                },
+                    html = '',
+                    sortedkeys = [];
+                html += '<' + element.name;
+                for (var key in element.attributes)
+                    sortedkeys.push(key);
+                sortedkeys.sort();
+                for (var m in sortedkeys)
+                    html += ' ' + sortedkeys[m] + '="' + element.attributes[sortedkeys[m]] + '"';
+                html += '></' + element.name + '>';
                 selectors.push({
                     string: elements[i] + ids[j] + classes[k] + attributes[l],
                     name: elements[i] == '' ? 'div' : elements[i],
-                    id: ids[j].replace('#', ''),
+                    id: id,
                     classes: classArray,
                     attributes: attributesObjects[l],
-                    element: {
-                        name: elements[i] == '' ? 'div' : elements[i],
-                        id: ids[j].replace('#', ''),
-                        classes: classArray,
-                        attributes: attributesObjects[l],
-                        children: [],
-                    }
+                    element: element,
+                    html: html,
                 }); // Correct Order
                 selectors.push({
                     string: elements[i] + attributes[l] + classes[k] + ids[j],
                     name: elements[i] == '' ? 'div' : elements[i],
-                    id: ids[j].replace('#', ''),
+                    id: id,
                     classes: classArray,
                     attributes: attributesObjects[l],
-                    element: {
-                        name: elements[i] == '' ? 'div' : elements[i],
-                        id: ids[j].replace('#', ''),
-                        classes: classArray,
-                        attributes: attributesObjects[l],
-                        children: [],
-                    }
+                    element: element,
+                    html: html,
                 }); // Random Order
             }
