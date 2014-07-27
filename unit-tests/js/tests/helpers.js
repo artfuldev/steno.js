bce935b44c1bf3e0c942564d92d3d908eb0ebb71
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-27 15:31:26 +0530

    Reworked Helpers

--- unit-tests/js/tests/helpers.js
+++ unit-tests/js/tests/helpers.js
@@ -1,106 +1,97 @@
 ﻿/// <reference path="../lib/jquery-2.1.1.min.js" />
 /// <reference path="../lib/qunit-git.js" />
-/// <reference path="../project/zen-query.js" />
+/// <reference path="../project/zquery.js" />
 
-$Q = QUnit;
 
-var classes = ['','.dropdown', '.menu.dropdown','.dropdown-menu'];
-var ids = ['','#menu'];
-var elements = ['', 'div', 'p'];
-var attributes = [
-    '', '[data-attribute]', '[for=""]', '[href="github.com/#"]', '[title="Something about zQuery\'s Awesomeness"]',
-    '[filter][title="Something about zQuery\'s \\"Awesomeness>+^()\\""]', '[filter title="Something about zQuery\'s \\"Awesomeness>+^()\\"" class="haha" id="hehe"]'
-];
-var attributesObjects = [
-    {}, { 'data-attribute': '' }, { 'for': '' }, { href: 'github.com/#' }, { title: "Something about zQuery\'s Awesomeness" },
-    { filter: '', title: 'Something about zQuery\'s "Awesomeness>+^()"' }, { filter: '', title: 'Something about zQuery\'s "Awesomeness>+^()"', 'class': "haha", id: "hehe" }
-];
+// Namespaces
+window.$Q = QUnit;
+window.$H = window.Helpers = {};
 
-function initForSingle() {
-    var testObjects = [];
-    for (var i in elements) {
-        for (var j in ids) {
-            for (var k in classes) {
-                for (var l in attributes) {
-                    var classArray = $Z.trim(classes[k].replace(/\./g, ' ')).split(' '),
-                        attributesObject = $Z.extend(true, {}, attributesObjects[l]),
-                        id = ids[j].replace('#', '');
-                    if (classArray.length === 1 && classArray[0] === '') {
-                        classArray = [];
-                    } else {
-                        if (!attributesObject['class']) {
-                            attributesObject['class'] = classArray.join(' ');
-                        } else {
-                            attributesObject['class'] += ' ' + classArray.join(' ');
-                        }
-                    }
-                    if (id) {
-                        $Z.extend(true, attributesObject, { id: id });
-                    }
-                    var element = {
-                            parent: null,
-                            name: elements[i] == '' ? 'div' : elements[i],
-                            attributes: attributesObject,
-                            children: [],
-                        },
-                        html = '',
-                        sortedkeys = [];
-                    html += '<' + element.name;
-                    for (var key in element.attributes)
-                        sortedkeys.push(key);
-                    sortedkeys.sort();
-                    for (var m in sortedkeys)
-                        html += ' ' + sortedkeys[m] + '="' + element.attributes[sortedkeys[m]] + '"';
-                    html += '></' + element.name + '>';
-                    testObjects.push({
-                        string: elements[i] + ids[j] + classes[k] + attributes[l],
-                        name: elements[i] == '' ? 'div' : elements[i],
-                        id: id,
-                        classes: classArray,
-                        attributes: attributesObjects[l],
-                        element: element,
-                        html: html,
-                    }); // Correct Order
-                }
-            }
+// Validate Args
+$H.arguments = function() { return arguments; };
+
+// Classes
+$H.classes = {
+    '': '',
+    '.dropdown': 'dropdown',
+    '.menu.dropdown': 'menu dropdown',
+    '.dropdown-menu': 'dropdown-menu',
+    '.dropdown-menu.dropdown.menu': 'dropdown-menu dropdown menu'
+};
+
+// Attributes
+$H.attributes = {
+    '': {},
+    '[data-attribute]': {
+        'data-attribute': ''
+    },
+    '[for=""]': {
+         'for': ''
+    },
+    '[href="github.com/#"]': {
+         href: 'github.com/#'
+    },
+    '[title="Something about zQuery\'s Awesomeness"]': {
+         title: 'Something about zQuery\'s Awesomeness'
+    },
+    '[filter][title="Something about zQuery\'s \\"Awesomeness>+^()\\""]': {
+        filter: '',
+        title: 'Something about zQuery\'s \\"Awesomeness>+^()\\"'
+    },
+    '[filter title="Something about zQuery\'s \\"Awesomeness>+^()\\"" class="haha" id="hehe"]': {
+        filter: '',
+        title: 'Something about zQuery\'s \\"Awesomeness>+^()\\"',
+        'class': 'haha',
+        id: 'hehe'
+    }
+};
+
+// Elements
+$H.elements = {
+    'work': {
+        name: 'work',
+        attributes: {}
+    },
+    '#menu': {
+        name: 'div',
+        attributes: {
+            id: 'menu'
+        }
+    },
+    'div.class-name': {
+        name: 'div',
+        attributes: {
+            'class': 'class-name'
+        }
+    },
+    'p.go-to-hell[class="help-me"]': {
+        name: 'p',
+        attributes: {
+            'class': 'go-to-hell help-me'
+        }
+    },
+    'p#id.class': {
+        name: 'p',
+        attributes: {
+            id: 'id',
+            'class': 'class'
+        }
+    },
+    'p[hi="how" are="\\"you\\""]': {
+        name: 'p',
+        attributes: {
+            hi: 'how',
+            are: '\\"you\\"'
+        }
+    },
+    'p#id.class[data-attr da="gpo" hi="\\"help\\""]': {
+        name: 'p',
+        attributes: {
+            id: 'id',
+            'class': 'class',
+            'data-attr': '',
+            da: 'gpo',
+            hi: '\\"help\\"'
         }
     }
-    return testObjects;
-}
-
-function getRandom(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
-function initForMultiple() {
-
-    var testStrings = [],
-        selectors = [],
-        delimiters = ['+', ' ', '>', '^'],
-        pairs = '()';
-    for (var i in elements) {
-        for (var j in ids) {
-            for (var k in classes) {
-                for (var l in attributes) {
-                    selectors.push(elements[i] + ids[j] + classes[k] + attributes[l]); // Correct Order
-                }
-            }
-        }
-    }
-    for (var i = 0; i < 10; i++) {
-        var testString = '';
-        for (var j = 0; j < 8; j++) {
-            var element = getRandom(selectors);
-            var delimiter = getRandom(delimiters);
-            testString += element;
-            if (j < 3 || j == 5)
-                testString += delimiter;
-            if (j == 3 || j == 4)
-                testString += delimiter + '(';
-            if (j == 6 || j == 7)
-                testString += ')' + delimiter;
-        }
-        testStrings.push(testString);
-    }
-    return testStrings;
-}
+};
