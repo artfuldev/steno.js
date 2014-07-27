69dc9aae422e7c54395d6362ebd6240c9f35ca69
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-27 22:37:05 +0530

    Added more Unit Test Helpers

--- unit-tests/js/tests/helpers.js
+++ unit-tests/js/tests/helpers.js
@@ -14,12 +14,16 @@ $H.types = {
     'undefined': undefined,
     'object': {},
     'array': [],
-    'function': function () { },
+    'function': function() {},
     'string': '',
     'number': 12,
     'boolean': true,
     'date': new Date(),
-    'regexp': new RegExp()
+    'regexp': new RegExp(),
+    'string|number': 12,
+    'number|string': 12,
+    'array|object': [],
+    'object|array': []
 };
 
 // Has
@@ -31,7 +35,48 @@ $H.arr = [1, 2];
 $H.arr['key'] = 'value';
 
 // Validate Args
-$H.arguments = function() { return arguments; };
+$H.arguments = function () { return arguments; };
+$H.validate = {arguments:[],types:[]};
+for (var i in $H.types) {
+    $H.validate.arguments.push($H.arguments($H.types[i]));
+    $H.validate.types.push([i]);
+}
+
+// Trim
+$H.trim = {
+    ' Help': 'Help',
+    'Help ': 'Help',
+    ' Help ': 'Help'
+};
+
+// Nullify
+$H.nullify = [
+    {
+        arguments: [[0, null, undefined, 1, 2, 1, null], 0],
+        result: [0, 0, 0, 1, 2, 1, 0],
+        message: 'Works on arrays'
+    },
+    {
+        arguments: [{ hi: 0, there: null, how: undefined, are: 1, you: 2, doing: 1, hello: null }, 'nothing here'],
+        result: { hi: 0, there: 'nothing here', how: 'nothing here', are: 1, you: 2, doing: 1, hello: 'nothing here' },
+        message: 'Works on objects'
+    }
+];
+
+// Random
+$H.random = [
+    {
+        array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        test: function(a) { return a > 0 && a < 10; }
+    },
+    {
+        array: ['a', 'b', 'c'],
+        test: function(a) { return a === 'a' || a === 'b' || a === 'c'; }
+    }
+];
+
+// Extend
+// Needs Implementation
 
 // Classes
 $H.classes = {
