cbb7ec2e2bdfb484942fd0759fef54260de827e2
Kenshin The Battōsai (Sudarsan Balaji) <kenshin@thebattosai.in>
2014-07-31 00:10:00 +0530

    Unit Tests for Extend
    from jQuery
    https://github.com/jquery/jquery/blob/master/test/unit/core.js

--- unit-tests/js/tests/utilities.js
+++ unit-tests/js/tests/utilities.js
@@ -129,11 +129,7 @@ $Q.test('$Z.random', function(assert) {
     assert.ok(0 < $Z.random([1, 2, 3, 4, 5]) < 6, 'Correct Random Element Selection');
 });
 $Q.test("$Z.extend", function(assert) {
-    assert.expect(1);
-    assert.ok(true, 'Derived from the heavily-tested jQuery.extend, so skipped');
-});
-$Q.test("$Z.extend2", function(assert) {
-    //assert.expect(2);
+    assert.expect(30);
 
     var empty, optionsWithLength, optionsWithDate, myKlass,
 		customObject, optionsWithCustomObject, MyNumber, ret,
@@ -150,96 +146,105 @@ $Q.test("$Z.extend2", function(assert) {
 		arr = [1, 2, 3],
 		nestedarray = { "arr": arr };
 
-    $Z.extend2(settings, options);
+    $Z.extend(settings, options);
     assert.deepEqual(settings, merged, "Check if extended: settings must be extended");
     assert.deepEqual(options, optionsCopy, "Check if not modified: options must not be modified");
 
-    $Z.extend2(settings, null, options);
+    $Z.extend(settings, null, options);
     assert.deepEqual(settings, merged, "Check if extended: settings must be extended");
     assert.deepEqual(options, optionsCopy, "Check if not modified: options must not be modified");
 
-    $Z.extend2(true, deep1, deep2);
+    $Z.extend(true, deep1, deep2);
     assert.deepEqual(deep1["foo"], deepmerged["foo"], "Check if foo: settings must be extended");
-    //assert.deepEqual(deep2["foo"], deep2copy["foo"], "Check if not deep2: options must not be modified");
-    //assert.equal(deep1["foo2"], document, "Make sure that a deep clone was not attempted on the document");
+    assert.deepEqual(deep2["foo"], deep2copy["foo"], "Check if not deep2: options must not be modified");
+    assert.equal(deep1["foo2"], document, "Make sure that a deep clone was not attempted on the document");
 
-    //ok(jQuery.extend(true, {}, nestedarray)["arr"] !== arr, "Deep extend of object must clone child array");
+    assert.ok($Z.extend(true, {}, nestedarray)["arr"] !== arr, "Deep extend of object must clone child array");
 
-    //// #5991
-    //ok(jQuery.isArray(jQuery.extend(true, { "arr": {} }, nestedarray)["arr"]), "Cloned array have to be an Array");
-    //ok(jQuery.isPlainObject(jQuery.extend(true, { "arr": arr }, { "arr": {} })["arr"]), "Cloned object have to be an plain object");
+    assert.ok($Z.is('array',$Z.extend(true, { "arr": {} }, nestedarray)["arr"]), "Cloned array has to be an Array");
+    assert.ok($Z.is('plain object', $Z.extend(true, { "arr": arr }, { "arr": {} })["arr"]), "Cloned object has to be an plain object");
 
-    //empty = {};
-    //optionsWithLength = { "foo": { "length": -1 } };
-    //jQuery.extend(true, empty, optionsWithLength);
-    //deepEqual(empty["foo"], optionsWithLength["foo"], "The length property must copy correctly");
+    empty = {};
+    optionsWithLength = { "foo": { "length": -1 } };
+    $Z.extend(true, empty, optionsWithLength);
+    assert.deepEqual(empty["foo"], optionsWithLength["foo"], "The length property must copy correctly");
 
-    //empty = {};
-    //optionsWithDate = { "foo": { "date": new Date() } };
-    //jQuery.extend(true, empty, optionsWithDate);
-    //deepEqual(empty["foo"], optionsWithDate["foo"], "Dates copy correctly");
+    empty = {};
+    optionsWithDate = { "foo": { "date": new Date() } };
+    $Z.extend(true, empty, optionsWithDate);
+    assert.deepEqual(empty["foo"], optionsWithDate["foo"], "Dates copy correctly");
 
-    ///** @constructor */
-    //myKlass = function () { };
-    //customObject = new myKlass();
-    //optionsWithCustomObject = { "foo": { "date": customObject } };
-    //empty = {};
-    //jQuery.extend(true, empty, optionsWithCustomObject);
-    //ok(empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly (no methods)");
+    /** @constructor */
+    myKlass = function () { };
+    customObject = new myKlass();
+    optionsWithCustomObject = { "foo": { "date": customObject } };
+    empty = {};
+    $Z.extend(true, empty, optionsWithCustomObject);
+    assert.ok(empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly (no methods)");
 
-    //// Makes the class a little more realistic
-    //myKlass.prototype = { "someMethod": function () { } };
-    //empty = {};
-    //jQuery.extend(true, empty, optionsWithCustomObject);
-    //ok(empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly");
+    // Makes the class a little more realistic
+    myKlass.prototype = { "someMethod": function () { } };
+    empty = {};
+    $Z.extend(true, empty, optionsWithCustomObject);
+    assert.ok(empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly");
 
-    //MyNumber = Number;
+    MyNumber = Number;
 
-    //ret = jQuery.extend(true, { "foo": 4 }, { "foo": new MyNumber(5) });
-    //ok(parseInt(ret.foo, 10) === 5, "Wrapped numbers copy correctly");
+    ret = $Z.extend(true, { "foo": 4 }, { "foo": new MyNumber(5) });
+    assert.ok(parseInt(ret.foo, 10) === 5, "Wrapped numbers copy correctly");
 
-    //nullUndef = jQuery.extend({}, options, { "xnumber2": null });
-    //ok(nullUndef["xnumber2"] === null, "Check to make sure null values are copied");
+    nullUndef = $Z.extend({}, options, { "xnumber2": null });
+    assert.ok(nullUndef["xnumber2"] === null, "Check to make sure null values are copied");
 
-    //nullUndef = jQuery.extend({}, options, { "xnumber2": undefined });
-    //ok(nullUndef["xnumber2"] === options["xnumber2"], "Check to make sure undefined values are not copied");
+    nullUndef = $Z.extend({}, options, { "xnumber2": undefined });
+    assert.ok(nullUndef["xnumber2"] === options["xnumber2"], "Check to make sure undefined values are not copied");
 
-    //nullUndef = jQuery.extend({}, options, { "xnumber0": null });
-    //ok(nullUndef["xnumber0"] === null, "Check to make sure null values are inserted");
+    nullUndef = $Z.extend({}, options, { "xnumber0": null });
+    assert.ok(nullUndef["xnumber0"] === null, "Check to make sure null values are inserted");
 
-    //target = {};
-    //recursive = { foo: target, bar: 5 };
-    //jQuery.extend(true, target, recursive);
-    //deepEqual(target, { bar: 5 }, "Check to make sure a recursive obj doesn't go never-ending loop by not copying it over");
+    target = {};
+    recursive = { foo: target, bar: 5 };
+    $Z.extend(true, target, recursive);
+    assert.deepEqual(target, { bar: 5 }, "Check to make sure a recursive obj doesn't go into a  never-ending loop by not copying it over");
 
-    //ret = jQuery.extend(true, { foo: [] }, { foo: [0] }); // 1907
-    //equal(ret.foo.length, 1, "Check to make sure a value with coercion 'false' copies over when necessary to fix #1907");
+    ret = $Z.extend(true, { foo: [] }, { foo: [0] }); // 1907
+    assert.equal(ret.foo.length, 1, "Check to make sure a value with coercion 'false' copies over when necessary to fix #1907");
 
-    //ret = jQuery.extend(true, { foo: "1,2,3" }, { foo: [1, 2, 3] });
-    //ok(typeof ret.foo !== "string", "Check to make sure values equal with coercion (but not actually equal) overwrite correctly");
+    ret = $Z.extend(true, { foo: "1,2,3" }, { foo: [1, 2, 3] });
+    assert.ok(typeof ret.foo !== "string", "Check to make sure values equal with coercion (but not actually equal) overwrite correctly");
 
-    //ret = jQuery.extend(true, { foo: "bar" }, { foo: null });
-    //ok(typeof ret.foo !== "undefined", "Make sure a null value doesn't crash with deep extend, for #1908");
+    ret = $Z.extend(true, { foo: "bar" }, { foo: null });
+    assert.ok(typeof ret.foo !== "undefined", "Make sure a null value doesn't crash with deep extend, for #1908");
 
-    //obj = { foo: null };
-    //jQuery.extend(true, obj, { foo: "notnull" });
-    //equal(obj.foo, "notnull", "Make sure a null value can be overwritten");
+    obj = { foo: null };
+    $Z.extend(true, obj, { foo: "notnull" });
+    assert.equal(obj.foo, "notnull", "Make sure a null value can be overwritten");
 
-    //function func() { }
-    //jQuery.extend(func, { key: "value" });
-    //equal(func.key, "value", "Verify a function can be extended");
+    function func() { }
+    $Z.extend(func, { key: "value" });
+    assert.equal(func.key, "value", "Verify a function can be extended");
 
-    //defaults = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
-    //defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
-    //options1 = { xnumber2: 1, xstring2: "x" };
-    //options1Copy = { xnumber2: 1, xstring2: "x" };
-    //options2 = { xstring2: "xx", xxx: "newstringx" };
-    //options2Copy = { xstring2: "xx", xxx: "newstringx" };
-    //merged2 = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "xx", xxx: "newstringx" };
+    defaults = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
+    defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
+    options1 = { xnumber2: 1, xstring2: "x" };
+    options1Copy = { xnumber2: 1, xstring2: "x" };
+    options2 = { xstring2: "xx", xxx: "newstringx" };
+    options2Copy = { xstring2: "xx", xxx: "newstringx" };
+    merged2 = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "xx", xxx: "newstringx" };
 
-    //settings = jQuery.extend({}, defaults, options1, options2);
-    //deepEqual(settings, merged2, "Check if extended: settings must be extended");
-    //deepEqual(defaults, defaultsCopy, "Check if not modified: options1 must not be modified");
-    //deepEqual(options1, options1Copy, "Check if not modified: options1 must not be modified");
-    //deepEqual(options2, options2Copy, "Check if not modified: options2 must not be modified");
+    settings = $Z.extend({}, defaults, options1, options2);
+    assert.deepEqual(settings, merged2, "Check if extended: settings must be extended");
+    assert.deepEqual(defaults, defaultsCopy, "Check if not modified: options1 must not be modified");
+    assert.deepEqual(options1, options1Copy, "Check if not modified: options1 must not be modified");
+    assert.deepEqual(options2, options2Copy, "Check if not modified: options2 must not be modified");
+
+    var result, initial = {
+        array: [1, 2, 3, 4],
+        object: {}
+    };
+
+    result = $Z.extend(true, {}, initial);
+
+    assert.deepEqual(result, initial, "The [result] and [initial] have equal shape and values");
+    assert.ok(!$Z.is('array', result.object), "result.object wasn't paved with an empty array");
 });
